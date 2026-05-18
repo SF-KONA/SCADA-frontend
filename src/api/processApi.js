86d218747387414119356718caae99a446c4ded2
kkmjung <kkmjung@gmail.com>
2026-05-18 11:34:15 +0900

    feat: 설비 상세 페이지 구현

--- src/api/processApi.js
+++ src/api/processApi.js
@@ -3,3 +3,17 @@ import api from './index.js'
 export const getProcessList = () => api.get('/processes')
 
 export const getPartnerDetail = (stepNo) => api.get(`/processes/${stepNo}/partner`)
+
+export const getEquipmentList = (stepNo) => api.get(`/processes/${stepNo}/equipments`)
+
+export const getEquipmentParameters = (equipmentId, period) =>
+  api.get(`/equipments/${equipmentId}/parameters`, { params: { period } })
+
+export const getEquipmentAlarms = (equipmentId, status) =>
+  api.get(`/equipments/${equipmentId}/alarms`, { params: { status } })
+
+export const getEquipmentEvents = (equipmentId, page, size) =>
+  api.get(`/equipments/${equipmentId}/events`, { params: { page, size } })
+
+export const addEquipmentNote = (equipmentId, noteText) =>
+  api.post(`/equipments/${equipmentId}/notes`, { noteText })
